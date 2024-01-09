ea3d33752323627622d4862d48365abaf4ab3cc5
Noval Agung Prayogo <hello@novalagung.com>
2024-01-09 22:59:29 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,9 +11,9 @@ const config = buildConfig({
   seoKeywords: 'python, belajar python, pemrograman python, noval agung, data analytics, fundamental python, contoh python, data science, machine learning',
   googleTrackingID: 'G-JLWG9B72SF',
   facebookAppID: '875100400724859',
-  algoliaAppID: '',
-  algoliaApiKey: '',
-  algoliaIndexName: '',
+  algoliaAppID: 'GTSLZ9HJP5',
+  algoliaApiKey: 'd4cb065e748f1f0bde373e31b8f155a0',
+  algoliaIndexName: 'dasarpemrogramanpython-novalagung',
 });
 
 module.exports = config;
